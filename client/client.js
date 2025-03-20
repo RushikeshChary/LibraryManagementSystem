493fa9a28ea7b7865619411c6790005cec625dee
RushikeshChary <ruushid19@gmail.com>
2025-03-21 01:04:14 +0530

    client initial template

--- client/client.js
+++ client/client.js
@@ -6,13 +6,57 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-function fetchBooks() {
+function viewBooks() {
     axios.get('http://localhost:3000/users')
         .then(response => console.log(response.data))
-        .catch(error => console.error('Error:', error));
+        .catch(error => console.error('Error:', error))
+        .finally(() => mainMenu());
 }
 
-rl.question('Enter command (list-users): ', (command) => {
-    if (command === 'list-users') fetchBooks();
-    rl.close();
-});
+
+// Function to get user input as a promise
+function askQuestion(query) {
+    return new Promise(resolve => rl.question(query, resolve));
+}
+
+
+
+async function mainMenu() {
+    console.log('\nLibrary Management System');
+    console.log('1. Add Book');
+    console.log('2. View Books');
+    console.log('3. Search Book');
+    console.log('4. Update Book');
+    console.log('5. Delete Book');
+    console.log('6. Exit');
+
+    const choice = await askQuestion('Enter your choice: ');
+
+    switch (choice) {
+        case '1': 
+            console.log("Add book function not implemented yet.");
+            break;
+        case '2': 
+            viewBooks(); 
+            return;
+        case '3': 
+            console.log("Search book function not implemented yet.");
+            break;
+        case '4': 
+            console.log("Update book function not implemented yet.");
+            break;
+        case '5': 
+            console.log("Delete book function not implemented yet.");
+            break;
+        case '6': 
+            console.log("Exiting...");
+            rl.close();
+            return;
+        default: 
+            console.log('Invalid choice!');
+    }
+
+    mainMenu(); // Call again to continue the loop
+}
+
+mainMenu();
