43da83f789784749f1013f7ab064dc242c5166ba
RushikeshChary <ruushid19@gmail.com>
2025-03-22 00:07:41 +0530

    book search implemented

--- server/routes/book.js
+++ server/routes/book.js
@@ -3,11 +3,38 @@ import db from "../db/connection.js";
 // const db = require("../db/connection.js");
 const router = express.Router();
 
-// Endpoint for books
-// 1. view books
-router.get('/view', async (req, res)=>{
+// Endpoint for searching books.
+router.get('/category/:cat_name', async (req, res)=>{
     try{
-        const [rows] = await db.query('SELECT * FROM books');
+        const { cat_name } = req.params;
+        const query = "SELECT book_id, book_title FROM book as b,category as c WHERE b.category_id = c.category_id AND c.category_name =?";
+        const [rows] = await db.query(query,[cat_name]);
+        res.json(rows);
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).json({ message: 'Error retrieving books' });
+    }
+})
+
+router.get('/title/:title_name',async (req,res)=>{
+    try{
+        const { title_name } = req.params;
+        const query = "SELECT book_id, book_title FROM book WHERE book_title =?"
+        const [rows] = await db.query(query, [title_name]);
+        res.json(rows);
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).json({ message: 'Error retrieving books' });
+    }
+})
+
+router.get('/author/:author_name',async (req,res)=>{
+    try{
+        const { author_name } = req.params;
+        const query = "SELECT b.book_id, b.book_title FROM book as b, book_author as ba, author as a WHERE b.book_id = ba.book_id AND a.author_id = ba.author_id AND a.author_name =?";
+        const [rows] = await db.query(query, [author_name]);
         res.json(rows);
     }
     catch(err){
