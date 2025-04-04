2287ceda5e38508d944f517b911f0ea1d94c5741
RushikeshChary <ruushid19@gmail.com>
2025-04-05 00:14:42 +0530

    manager table added to database

--- data.sql
+++ data.sql
@@ -80,6 +80,13 @@ CREATE TABLE fine_due(
     FOREIGN KEY (user_id) REFERENCES user(user_id)
 );
 
+CREATE TABLE manager(
+    manager_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    mobile_no VARCHAR(255) UNIQUE NOT NULL
+);
 
 -- Create a triggers
 -- For filling fines.
